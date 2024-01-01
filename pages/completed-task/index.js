5e3fb6f7307a0bff8ae9160ae2c7bd8fbf973d7a
Naveen5676 <sample.first@yahoo.com>
2024-01-01 10:05:51 +0530

    Reflecting on the accomplishments

--- pages/completed-task/index.js
+++ pages/completed-task/index.js
@@ -1,4 +1,28 @@
+import { MongoClient } from "mongodb";
 import Completedtodos from "../../components/Todos/Completedtodos";
-export default function Completedtask(){
-return <Completedtodos/>
+
+export default function Completedtask(props){
+return <Completedtodos completedtodos={props.todos}/>
+}
+
+export async function getStaticProps(props){
+
+    const client = await MongoClient.connect(
+        "mongodb+srv://new-user31:EXEY2T9sVmiwh1Nm@cluster0.pepc9yq.mongodb.net/meetups?retryWrites=true&w=majority"
+      );
+      const db = client.db();
+    
+      const todosdata = db.collection("todos");
+    
+      const todos = await todosdata.find({completed:true}).toArray();
+      client.close();
+
+    return{
+        props:{
+            todos: todos.map((todo)=>({
+                task:todo.task,
+                id: todo._id.toString()
+            }))
+        },
+    };
 }
